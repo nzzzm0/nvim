f7c02a01e19bbb763241ee934fc8b492092d6c62
nzzzm0 <142912369+nzzzm0@users.noreply.github.com>
2024-06-29 22:16:44 +0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+
+
