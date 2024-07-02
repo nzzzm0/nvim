b0ea7b399b56650e3268993b5a134030987d5a01
hpnzm <theweedsmokers69@gmail.com>
2024-07-02 10:49:46 +0700

    lsp hover definition

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,19 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local map = vim.keymap.set
 
 local lspconfig = require('lspconfig')
 
 local servers = { 'gopls', 'tsserver'}
 
+local on_attach = function()
+  map("n","K",vim.lsp.buf.hover,{})
+  map("n","rn",vim.lsp.buf.rename,{})
+end
+
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
+    on_attach = on_attach,
     capabilities = capabilities,
   }
 end
